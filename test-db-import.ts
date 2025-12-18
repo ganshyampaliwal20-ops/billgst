350cf3b5119c2de25062da0eca9e34882cacbc7e
ganshyam paliwal <gpaliwal59@gmail.com>
2025-12-18 11:25:34 +0530

    Remove debug scripts to fix Vercel build error

--- test-db-import.ts
+++
@@ -1,6 +0,0 @@
-
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env.local' });
-console.log('Importing lib/db.ts...');
-import pool from './lib/db.ts';
-console.log('Import successful. Pool:', pool ? 'Defined' : 'Undefined');
