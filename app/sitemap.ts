bb44c390825ae91a297c06ee97b78713760bcf19
ganshyam paliwal <gpaliwal59@gmail.com>
2025-12-28 06:59:40 +0530

    SEO: Fix sitemap domain error - forced billgst.in canonical url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = process.env.NEXT_PUBLIC_APP_URL
-        ? process.env.NEXT_PUBLIC_APP_URL
-        : process.env.VERCEL_URL
-            ? `https://${process.env.VERCEL_URL}`
-            : 'https://billgst.in';
+    const baseUrl = 'https://billgst.in';
 
     return [
         {
