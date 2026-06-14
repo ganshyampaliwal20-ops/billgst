7499db15d5fbc3b44ca5dc150225aef8d1c5a035
Ganshyam paliwal <ganshyampaliwal20@gmail.com>
2026-06-14 17:31:53 +0530

    Cleanup: Remove temporary scripts

--- fix_syntax.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('f:/bill/app/page.tsx', 'utf8');
-c = c.replace('allowFullScreen loading=" lazy\\', 'allowFullScreen loading="lazy"');
-fs.writeFileSync('f:/bill/app/page.tsx', c, 'utf8');
-console.log('Fixed syntax!');
